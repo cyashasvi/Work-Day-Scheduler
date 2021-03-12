289f4a8bde2eba318dafaa9cdd9a59e515e1cb53
cyashasvi <cyashasvi@gmail.com>
2021-03-11 23:48:23 -0800

    made intermediate changes

---
+++ Develop/javascript.js
@@ -0,0 +1,51 @@
+function showTime() {
+    var time = moment().format('MMMM Do YYYY [at] h:mm:ss a') // add square brackets to create the [at] in the moment date and time
+        $("#currentDay").text(time)
+    
+}
+setInterval(showTime, 1000)
+
+var times = {
+    nine: document.getElementById('9').innerHTML,
+    ten :document.getElementById('10').innerHTML,
+    eleven: document.getElementById('11').innerHTML,
+    twelve: document.getElementById('12').innerHTML,
+    one: document.getElementById('1').innerHTML,
+    two: document.getElementById('2').innerHTML,
+    three: document.getElementById('3').innerHTML,
+    four: document.getElementById('4').innerHTML,
+    five: document.getElementById('5').innerHTML
+
+}
+
+
+var currentHour = moment().format('h')
+console.log(currentHour)
+var timeEl = document.getElementsByClassName('input-group-text') 
+
+//$.each(times,function colorSwitcher(key,value) {
+   // console.log(key)
+   // if (currentHour > value) {
+       // $('input-group-text').classList.add('future');
+//    }
+//}); 
+var inputs = {
+    input9: document.querySelector('#ninetxt').value.trim(),
+    input10: document.querySelector('#tentxt').value.trim(),
+    input11: document.querySelector('#eleventxt').value.trim(),
+    input12: document.querySelector('#twelvetxt').value.trim(),
+    input1: document.querySelector('#onetxt').value.trim(),
+    input2: document.querySelector('#twotxt').value.trim(),
+    input3: document.querySelector('#threetxt').value.trim(),
+    input4: document.querySelector('#fourtxt').value.trim(),
+    input5: document.querySelector('#fivetxt').value.trim(),
+}
+
+function saveData(){
+    localStorage.setItem('planner', JSON.stringify(inputs));
+}
+
+document.getElementById('button-addon2').addEventListener('click',function(event) {
+    event.preventDefault();
+    saveData();
+})
